8ebe3d8d79aa0959eb05dcdb5807564fdeffc481
xianlechuanshuo <369310235@qq.com>
2017-07-22 12:12:29 +0800

    修改js代码，兼容IE8，现IE8也能正常使用

--- common.js
+++ common.js
@@ -1,6 +1,8 @@
-﻿function $(id) {
+﻿//根据id获取dom对象
+function $(id) {
     return document.getElementById(id);
 }
+//根据标签获取dom集合
 function $$(tagname) {
     return document.getElementsByTagName(tagname);
 }
@@ -16,13 +18,15 @@ function getDdlText(id) {
     var text = $(id).options[index].text;
     return text;
 }
+
+//获取下拉列表选中项的对应属性的值
 function getDdlAttrText(id,name) {
     var index = $(id).selectedIndex;
     var text = $(id).options[index].getAttribute(name);
     return text;
 }
 
-// Array Remove - By John Resig (MIT Licensed)
+// 对数组的原型添加remove方法
 Array.prototype.remove = function (from, to) {
     var rest = this.slice((to || from) + 1 || this.length);
     this.length = from < 0 ? this.length + from : from;
@@ -36,27 +40,50 @@ function compare(property) {
         return value2 - value1;
     }
 }
+//父容器中，使其自适应内容高度
+function resizeWindow() {
+    //IE中：document.frames['iframe的name'].document.getElementById('元素的ID');
+    //非IE中：window.frames["iContent"].contentWindow.document.getElementById('content')
+    //iframe的id与name不能一样
+    //var height = window.frames["iContent"].contentWindow ? window.frames["iContent"].contentWindow.document.getElementById('content').offsetHeight : document.frames['iContentN'].document.getElementById('content').offsetHeight;
 
-//使父窗口iContent自适应内容高度
-function resizeParentWindow() {
-    var height = $("content").offsetHeight;//获取body的高度
-    var iContent = window.parent.frames["iContent"];
+    //通用方法：通过contentWindow获取到内容页面的高度
+    var height = $('iContent').contentWindow.document.getElementById('content').offsetHeight;
     if (iContent) {
-        iContent.style.height = (height + 60) + "px";
+        $("iContent").style.height = (height + 80) + "px";
     }
 }
 
+//内容页中，使父窗口iContent自适应内容高度
+function resizeParentWindow() {
+    var height = $("content").offsetHeight;//获取body的高度
+    //var iContent = window.parent.frames["iContent"] ;
+    //if (iContent) {
+    //    iContent.style.height = (height + 60) + "px";
+    //}
+
+    //获取到父容器iContent的dom对象
+    var iContent = window.parent.document.getElementById('iContent');
+    if (iContent) {
+        iContent.style.height = (height + 80) + "px";
+    }
+}
+
+//判断是否拥有某个类
 function hasClass(elem, cls) {
     cls = cls || '';
     if (cls.replace(/\s/g, '').length == 0) return false; //当cls没有参数时，返回false
     return new RegExp(' ' + cls + ' ').test(' ' + elem.className + ' ');
 }
 
+//添加类
 function addClass(ele, cls) {
     if (!hasClass(ele, cls)) {
         ele.className = ele.className == '' ? cls : ele.className + ' ' + cls;
     }
 }
+
+//移除类
 function removeClass(ele, cls) {
     if (hasClass(ele, cls)) {
         var newClass = ' ' + ele.className.replace(/[\t\r\n]/g, '') + ' ';
