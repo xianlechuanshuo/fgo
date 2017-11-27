8fdd4f7ed2ff7838fb6c74821c89a5d12b916415
xianlechuanshuo <369310235@qq.com>
2017-11-27 21:29:05 +0800

    测试2

--- js/base.js
+++ js/base.js
@@ -254,13 +254,13 @@ function bindSearchTips(){
         dlTips.appendChild(opt);
     })
 }
-(function(){
-    var nums=document.querySelectorAll("#txtCardBuff,#txtAttackBuff,#txtEnemyDefenceBuff");
-    nums.forEach(function(n){
-        n.type="text";
-        n.onblur=function(){
-            //失去焦点后，计算值
-            this.value=eval2(this.value);   
-        }
-    })
-})();
+// (function(){
+//     var nums=document.querySelectorAll("#txtCardBuff,#txtAttackBuff,#txtEnemyDefenceBuff");
+//     nums.forEach(function(n){
+//         n.type="text";
+//         n.onblur=function(){
+//             //失去焦点后，计算值
+//             this.value=eval2(this.value);   
+//         }
+//     })
+// })();
