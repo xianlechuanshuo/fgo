c4599b42ed6c67d3293ee9c1e98094197db0b02c
xianlechuanshuo <369310235@qq.com>
2019-08-16 18:20:03 +0800

    无

--- updatescripts/更新NP获取率js脚本，这玩意跟我签名中那个油猴插件有关，也跟计算器中NP获取图有关.js
+++ updatescripts/更新NP获取率js脚本，这玩意跟我签名中那个油猴插件有关，也跟计算器中NP获取图有关.js
@@ -13,18 +13,28 @@
 
             // {"artHit":2,"busterHit":1,"quickHit":3,"exHit":4,"npbase":71,"npbaseA":71,"npbaseB":71,"npbaseQ":71,"npbaseEx":71,"npUp":0,"artUp":50,"busterUp":0,"quickUp":0,"npHit":5,"npType":1}            
             // if(no==132||no==142||no==133)console.log(JSON.stringify(a));//输出R小莫(npType1)、弓凛、杀师匠的np基础数据
-            if(no==47||no==48||no==46){
-                console.log(`${no}：${JSON.stringify(a)}`);
-                let tmp=a.npType==1?"A":(a.npType==2?"Q":"B");
-                console.log(tmp);
-            }
 
-            //npType:1是蓝宝具、2是绿宝具、3是红宝具
+            switch(a.npType){
+                case 1:
+                  a.npType="A";
+                  break;
+                case 2:
+                  a.npType="B";
+                  break;
+                case 3:
+                  a.npType="Q";
+                  break;
+            }
+            if(no==46||no==47||no==48||no==52){
+                console.log(`${no}：${JSON.stringify(a)}`);
+                console.log(a.npType);
+            }            
+            //npType:1是蓝宝具、2是红宝具、3是绿宝具
         	var npObj={
         		n:no,//从者编号
         		h:a.npHit,//宝具Hit数
                          np:a.npbase/100,//宝具NP率
-                         t:a.npType==1?"A":(a.npType==2?"Q":"B"),
+                         t:a.npType,
 
                          ah:a.artHit,//A卡Hit次数
                          anp:a.npbaseA/100,//A卡NP率
